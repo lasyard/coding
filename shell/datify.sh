2410b919467f222500fc6becc9bacdeab6bf5f8c
Lasy <lasyard@yeah.net>
2024-01-11 14:00:11 +0800

    [shell] Use long year format in datify.

--- shell/datify.sh
+++ shell/datify.sh
@@ -7,7 +7,7 @@ fi
 
 for file in "$@"; do
     if [[ ${file} =~ [0-9]{10} ]]; then
-        name="$(date -j -f "%s" "+%y%m%d_%H%M%S" "$(expr "${file%.*}" : "[^0-9]*\([0-9]\{10\}\)")")"."${file##*.}"
+        name="$(date -j -f "%s" "+%Y%m%d_%H%M%S" "$(expr "${file%.*}" : "[^0-9]*\([0-9]\{10\}\)")")"."${file##*.}"
         echo "Rename \"${file}\" to \"${name}\"."
         mv "${file}" "${name}"
     fi
